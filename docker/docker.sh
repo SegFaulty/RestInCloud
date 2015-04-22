1d5f1127cf79cbc092818692043cfbda8ddee942
SegFaulty <zekvoult@gmail.com>
2015-04-22 22:06:40 +0100

    docker init

--- docker/docker.sh
+++ docker/docker.sh
@@ -5,11 +5,15 @@ rm -rf tmpBuild
 mkdir -p tmpBuild
 cp Dockerfile tmpBuild/
 cp php.ini tmpBuild/
+cp config.json tmpBuild/
 cd tmpBuild
 
 git clone https://github.com/SegFaulty/RestInCloud.git
 
-docker build .
+docker build -t ric-server .
 
-echo "docker run -it -p 3070:3070 [ConId] /bin/bash"
-echo "then opne your browser http://ip:3070/"
+echo "docker run -d -p 3070:3070 ric-server"
+echo "or for debuging:"
+echo "docker run -it -p 3070:3070 ric-server /bin/bash"
+echo "then point your browser to http://containerIp:3070/"
+echo "change config.json and rerun to inject a config"
