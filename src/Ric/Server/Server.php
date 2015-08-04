e3723f4b548fe7c5c76c44d62bbc005c5f2060af
sascha.weinmann <sascha.weinmann@motor-talk-gmbh.de>
2015-08-04 11:52:44 +0200

    WORK IN PROGRESS
    Move regex check to new Helper

--- src/Ric/Server/Server.php
+++ src/Ric/Server/Server.php
@@ -1012,54 +1012,8 @@ class Ric_Server_Server {
 	 * @throws RuntimeException
 	 */
 	static protected function validateRegex($regEx){
-		// check is valid regex
-		set_error_handler('Ric_Server_ErrorHandler_Static::handleError');
-		Ric_Server_ErrorHandler_Static::reset();
-		preg_match($regEx,'');
-		$error = Ric_Server_ErrorHandler_Static::$errstr;
-		restore_error_handler();
-		if( $error ){
-			throw new RuntimeException('not a valid regex: '.$error, 400);
-		}
-		return $regEx;
+        return Ric_Server_Helper_RegexPatternValidation::validateRegex($regEx);
 	}
 
 
-}
-
-class Ric_Server_ErrorHandler_Static {
-
-	static public  $errno = 0;
-	static public  $errstr = '';
-	static public  $errfile = '';
-	static public  $errline = '';
-	static public  $errcontext = array();
-
-	/**
-	 * @param int $errno
-	 * @param string $errstr
-	 * @param string $errfile
-	 * @param int $errline
-	 * @param array $errcontext
-	 * @return bool
-	 */
-	static public function handleError($errno, $errstr, $errfile, $errline, $errcontext){
-		self::$errno = $errno;
-		self::$errstr = $errstr;
-		self::$errfile = $errfile;
-		self::$errline = $errline;
-		self::$errcontext = $errcontext;
-		return true;
-	}
-
-	/**
-	 * reset last error
-	 */
-	static public function reset(){
-		self::$errno = 0;
-		self::$errstr = '';
-		self::$errfile = '';
-		self::$errline = 0;
-		self::$errcontext = array();
-	}
 }
