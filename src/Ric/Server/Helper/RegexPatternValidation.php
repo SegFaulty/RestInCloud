25d9de96fa8ab59bd0ae929635037888d82dc7b0
sascha.weinmann <sascha.weinmann@motor-talk-gmbh.de>
2015-08-04 12:03:37 +0200

    WORK IN PROGRESS
    Refactor checking for error
    Remove unnecessary code

--- src/Ric/Server/Helper/RegexPatternValidation.php
+++ src/Ric/Server/Helper/RegexPatternValidation.php
@@ -1,41 +1,6 @@
 <?php
 
 class Ric_Server_Helper_RegexPatternValidation{
-
-    static public  $errno = 0;
-    static public  $errstr = '';
-    static public  $errfile = '';
-    static public  $errline = '';
-    static public  $errcontext = array();
-
-    /**
-     * @param int $errno
-     * @param string $errstr
-     * @param string $errfile
-     * @param int $errline
-     * @param array $errcontext
-     * @return bool
-     */
-    static public function handleError($errno, $errstr, $errfile, $errline, $errcontext){
-        self::$errno = $errno;
-        self::$errstr = $errstr;
-        self::$errfile = $errfile;
-        self::$errline = $errline;
-        self::$errcontext = $errcontext;
-        return true;
-    }
-
-    /**
-     * reset last error
-     */
-    static public function reset(){
-        self::$errno = 0;
-        self::$errstr = '';
-        self::$errfile = '';
-        self::$errline = 0;
-        self::$errcontext = array();
-    }
-
     /**
      * return same regEx if valid
      * or throws Exception if not valid
@@ -45,51 +10,15 @@ class Ric_Server_Helper_RegexPatternValidation{
      */
     static public function validateRegex($regEx){
         // check is valid regex
-        set_error_handler('Ric_Server_ErrorHandler_Static::handleError');
-        Ric_Server_ErrorHandler_Static::reset();
+        //$errorString = '';
+        set_error_handler(function($errno, $errstr, $errfile, $errline, $errcontext) use(&$errorString){
+            $errorString = $errstr;
+        });
         preg_match($regEx,'');
-        $error = Ric_Server_ErrorHandler_Static::$errstr;
         restore_error_handler();
-        if( $error ){
-            throw new RuntimeException('not a valid regex: '.$error, 400);
+        if($errorString){
+            throw new RuntimeException('not a valid regex: '.$errorString, 400);
         }
         return $regEx;
-    }
-}
-
-class Ric_Server_ErrorHandler_Static {
-
-    static public  $errno = 0;
-    static public  $errstr = '';
-    static public  $errfile = '';
-    static public  $errline = '';
-    static public  $errcontext = array();
-
-    /**
-     * @param int $errno
-     * @param string $errstr
-     * @param string $errfile
-     * @param int $errline
-     * @param array $errcontext
-     * @return bool
-     */
-    static public function handleError($errno, $errstr, $errfile, $errline, $errcontext){
-        self::$errno = $errno;
-        self::$errstr = $errstr;
-        self::$errfile = $errfile;
-        self::$errline = $errline;
-        self::$errcontext = $errcontext;
-        return true;
-    }
-
-    /**
-     * reset last error
-     */
-    static public function reset(){
-        self::$errno = 0;
-        self::$errstr = '';
-        self::$errfile = '';
-        self::$errline = 0;
-        self::$errcontext = array();
     }
 }
