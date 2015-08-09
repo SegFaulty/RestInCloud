84527aab252ccf36af3f901805f640de5064b3f1
sascha.weinmann <sascha.weinmann@motor-talk-gmbh.de>
2015-08-09 12:13:24 +0200

    Add unit tests

---
+++ tests/Test/Ric/Server/ServerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/../../../bootstrap.php';
+
+class Test_Ric_Server_ServerTest extends \PHPUnit_Framework_TestCase {
+    public function test(){
+        self::markTestIncomplete();
+    }
+}
