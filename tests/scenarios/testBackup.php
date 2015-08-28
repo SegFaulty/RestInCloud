bd15fda051c0a8bff181b3a8f3a995722cb43c83
Grain <s.schwuchow@schottenland.de>
2015-08-28 23:09:24 +0200

    update testBackup scenarios

--- tests/scenarios/testBackup.php
+++ tests/scenarios/testBackup.php
@@ -12,6 +12,7 @@ checkOK(Ric_Rest_Client::put($servers[0].'/testfile.txt?token=admin', $tesFileCo
 ln('check file');
 checkOk(Ric_Rest_Client::get($servers[0].'/testfile.txt?check&token=admin'));
 
+ln('test restore');
 $response = Ric_Rest_Client::get($servers[0].'/testfile.txt?&token=admin');
 check($response===$tesFileContent, 'file content is not identically');
 
@@ -19,6 +20,9 @@ check($response===$tesFileContent, 'file content is not identically');
 $result = unJson(Ric_Rest_Client::delete($servers[0].'/testfile.txt?token=admin'));
 check($result['filesDeleted']=='3', 'delete failed for rest testfiles, expected 3  response:'.print_r($result, true));
 
+ln('test fail with retention at post');
+$result = unJson(Ric_Rest_Client::post($servers[0].'/testfile.txt?retention=5l&token=admin'));
+check($result['error']=='parameter retention is not allowed on post action', 'failed: post with retention is not okay '.print_r($result, true));
 
 
 ln('backup test passed');
