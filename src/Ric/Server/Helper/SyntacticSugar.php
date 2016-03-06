28d2ede30b3b4537189d6200623e400ef57beb03
Grain <s.schwuchow@schottenland.de>
2016-03-06 23:06:01 +0100

    update syntactic sugar function getIKS to work multi-level key

--- src/Ric/Server/Helper/SyntacticSugar.php
+++ src/Ric/Server/Helper/SyntacticSugar.php
@@ -2,7 +2,27 @@
 
 // Helper syntactic sugar function
 class H{
-/* getKeyIfSet */ static public function getIKS(&$array, $key, $default=null){return (array_key_exists($key, $array) ? $array[$key] : $default );}
+/* getKeyIfSet */
+	/**
+	 * Note: No notice is raised if the variable is not defined
+	 * and the variable is still not defined after this step
+	 * @param array $array
+	 * @param int|string|int[]|string[] $keys
+	 * @param mixed $default
+	 * @return mixed
+	 */
+	static public function getIKS(&$array, $keys, $default=null){
+		$result = $default;
+		$keys = (array) $keys;
+		$key = array_shift($keys);
+		if( array_key_exists($key, $array) ){
+			$result = $array[$key];
+			if( count($keys) ){
+				$result = self::getIKS($result, $keys, $default);
+			}
+		}
+		return $result;
+	}
 /* getRequestParameter */ static public function getRP($key, $default=null){return H::getIKS($_REQUEST, $key, $default);}
 /* json_encode */ static public function json($array){return json_encode($array, JSON_PRETTY_PRINT+JSON_UNESCAPED_SLASHES).PHP_EOL;}
 /* implodeKeyValue */ 	static public function implodeKeyValue($inputArray,$delimiter=', ',$keyValueDelimiter=': '){implode($delimiter,array_map(function($k,$v) use ($keyValueDelimiter)  {return $k.$keyValueDelimiter.$v;},array_keys($inputArray),$inputArray));}
