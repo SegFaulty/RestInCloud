bb8a77e88e8dd6e3334009bbcdbddd8de174958c
SegFaulty <zekvoult@gmail.com>
2019-04-25 15:19:40 +0200

    update phars server version to 0.7 client to 0.3

--- src/Ric/Dumper/phar/buildPhar.php
+++ src/Ric/Dumper/phar/buildPhar.php
@@ -5,18 +5,18 @@ if( $readonly && 'off'!==strtolower($readonly) ){
 	die(".phar creation disabled in php.ini!\nRe-run with \nphp -d phar.readonly=0 ".$argv[0]."\n");
 }
 
-$pharFile = 'dumper.phar';
+$pharFile = __DIR__.'/dumper.phar';
 if( file_exists($pharFile) ){
 	unlink($pharFile);
 }
 
-$phar = new Phar($pharFile, 0, $pharFile);
+$phar = new Phar($pharFile, 0, 'dumper.phar');
 $phar->setSignatureAlgorithm(Phar::SHA1);
 $phar->startBuffering();
-$phar->addFile('../Dumper.php', 'Dumper.php');
-$phar->addFile('../../Client/Cli.php', 'Cli.php');
-$phar->addFile('../../Server/Helper/SyntacticSugar.php', 'SyntacticSugar.php');
-$phar->addFile('../README.md', 'README.md');
+$phar->addFile(__DIR__.'/../Dumper.php', 'Dumper.php');
+$phar->addFile(__DIR__.'/../../Client/Cli.php', 'Cli.php');
+$phar->addFile(__DIR__.'/../../Server/Helper/SyntacticSugar.php', 'SyntacticSugar.php');
+$phar->addFile(__DIR__.'/../README.md', 'README.md');
 $stub = file_get_contents(__DIR__.'/dumper-phar-stub.php');
 $stub = str_replace('__LIVE__', date('Y-m-d H:i:s'), $stub); // inject build-date
 $phar->setStub($stub);
@@ -26,4 +26,4 @@ unset($phar);
 
 chmod($pharFile, 0755);
 echo 'done... test with:'.PHP_EOL;
-echo './'.$pharFile.' help --verbose'.PHP_EOL;
+echo $pharFile.' help --verbose'.PHP_EOL;
